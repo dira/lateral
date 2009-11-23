6c3b289d8f5ef3b7d46b19d2b3382083c7f4f3bf
Irina Dumitrascu <me@dira.ro>
2009-11-23 13:13:19 -0200

    it works!

---
+++ generate.rb
@@ -0,0 +1,17 @@
+def println(message)
+  print message, "\n"
+end
+
+inspiration = File.readlines('inspiration.txt')
+challenges =  File.readlines('challenges.txt')
+
+print 'Challenge: '
+println challenges[rand(challenges.size)]
+
+println "Rephrase it several times please!"
+STDIN.getc
+
+println "Inspiration:"
+(1..5).each do |i|
+  print "\t", inspiration.slice(rand(inspiration.size))
+end
